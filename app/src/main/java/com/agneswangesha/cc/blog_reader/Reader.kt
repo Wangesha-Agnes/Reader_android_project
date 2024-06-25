36282f71887f9f4b6484bf92dbf4a7ee9c582725
Wangesha-Agnes <wangeshaagnes@gmail.com>
2024-06-25 18:50:38 +0300

    made changes

--- app/src/main/java/com/agneswangesha/cc/blog_reader/Reader.kt
+++ app/src/main/java/com/agneswangesha/cc/blog_reader/Reader.kt
@@ -2,4 +2,11 @@ package com.agneswangesha.cc.blog_reader
 
 data class Reader(
     var author: String,
-)
+    var avatar: String,
+    var title: String,
+    var published: String,
+    var preview: String,
+    var photo: String,
+    var link: String,
+
+    )
